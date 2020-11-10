67d0f0a24baca70b4c9df8b2135bb6fe7af8128f
Tapsi Katyal <tkatyal@AffinionGroup.com>
2020-11-10 19:55:43 +0530

    Add project files.

---
+++ WcfService1/Person.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ServiceModel;
+
+namespace WcfService1
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string FirstName
+        {
+            get; set;
+        }
+        [DataMember]
+        public string LastName
+        {
+            get; set;
+        }
+        [DataMember]
+        public string ErrorMessage { get; set; }
+        [DataMember]
+        public string ErrorDetails
+        {
+            get; set;
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+       
+        public string GetMessage(string name)
+        {
+            return "Hello World " + name;
+        }
+
+        public Person GetPerson(Person person)
+        {
+            try
+            {
+                person.FirstName = null;
+                person.FirstName.ToString();
+                return new Person { FirstName = "FirstName is" + person.FirstName, LastName = "LastName is" + person.LastName };
+            }
+            catch (Exception ex)
+            {
+                person.ErrorDetails = ex.StackTrace;
+                person.ErrorMessage = "Retry, Error is coming";
+                throw new FaultException<Person>(person, new FaultReason("Invalid Routing Code - No Approval Started"));
+            }
+            
+        }
+    }
+}
